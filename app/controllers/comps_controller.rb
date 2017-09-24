7dc743656e01d4d5909b43cf656c60e6a5fe7b14
Mike Lerner <cheeseandpepper@gmail.com>
2017-09-24 11:05:29 -0700

    more ui stuff

--- app/controllers/comps_controller.rb
+++ app/controllers/comps_controller.rb
@@ -1,7 +1,7 @@
 class CompsController < ApplicationController
-
+  before_action :set_house
+  
   def index
-    @house = House.find(params[:house_id])
     @comps_link = "https://www.zillow.com/homes/comps/#{@house.house_ref}_zpid/"
     @comps_page = HTTParty.get(@comps_link, headers: {"User-Agent" => USER_AGENT})
     page = Nokogiri::XML(@comps_page)
@@ -17,6 +17,7 @@ class CompsController < ApplicationController
         image:         obj.css("img").first.attributes["src"].value,
         address:       obj.css(".listing-address").first.text.strip,
         price:         obj.css(".listing-price").text.strip,
+        comp_greater:  obj.css(".listing-price").text.strip.delete("Sold: $,").to_i > @house.price,
         more_diff:     obj.css(".facts-diff_more").text.strip,
         less_diff:     obj.css(".facts-diff_less").text.strip,
         details:       obj.css(".listing-details").text.strip,
@@ -29,5 +30,13 @@ class CompsController < ApplicationController
 
   def zillow_link_for(external_id)
     "https://www.zillow.com/homedetails/#{external_id}_zpid/"
+  end
+
+  def comp_price_is_greater(house, comp)
+    house.price 
+  end
+
+  def set_house
+    @house = House.find(params[:house_id])
   end
 end
